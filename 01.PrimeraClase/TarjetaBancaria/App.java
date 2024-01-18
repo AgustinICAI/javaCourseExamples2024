2ccbefb6c3db97fa626253a7e9cf920211a742d9
Agustín Gómez <a.g.lamela@gmail.com>
2024-01-18 09:38:46 +0100

    primera clase hecha en clase

---
+++ 01.PrimeraClase/TarjetaBancaria/App.java
@@ -0,0 +1,24 @@
+class App{
+
+  public static void main(String argv[]){
+    
+    TarjetaCredito t1 = new TarjetaCredito();
+    t1.setSaldo(2000);
+    t1.setNumeroTarjeta("1111 2222 3333 4444");
+    t1.setPin(1234);
+    t1.setCvv(333);
+    t1.setPinActivacion(1111);
+    
+    TarjetaCredito t2 = new TarjetaCredito("5555 5555 5555 5555");
+    
+    
+    
+    
+  
+  
+  
+  }
+
+
+
+}
---
+++ 01.PrimeraClase/TarjetaBancaria/Movimiento.java
@@ -0,0 +1,17 @@
+class Movimiento{
+  String establecimiento;
+  double importe;
+  String fecha;
+  
+  String getEstablecimiento(){
+    return establecimiento;
+  }
+  double getImporte(){
+    return importe;
+  }
+  String getFecha(){
+    return fecha;
+  }
+  
+
+}
