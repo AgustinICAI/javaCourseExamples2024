31b5e8f1f6f21c477c276b47d6001e7a2b48d22c
Agustín Gómez <a.g.lamela@gmail.com>
2024-04-04 09:41:57 +0200

    subiendo Mario Bros con hilos

---
+++ ejercicios_clase/08.1.JMarioBros-Threads/src/Sprite.java
@@ -0,0 +1,35 @@
+public abstract class Sprite {
+    int x;
+    int y;
+    int speed;
+
+    public Sprite(int x, int y, int speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
